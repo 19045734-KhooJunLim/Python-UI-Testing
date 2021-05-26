aa4f8f090ad2f6b0dd9c05fbe3b7a374503ced28
19045734 <19045734@myrp.edu.sg>
2021-05-26 16:12:39 +0800

    Basic Buttons

--- Python UI Testing/Python_UI_Testing.py
+++ Python UI Testing/Python_UI_Testing.py
@@ -3,4 +3,10 @@ from tkinter import *
 import tkinter as tk
 
 root = tk.Tk()
+root.title('UI Test')
+root.geometry('600x300')
 
+b1 = Button(root, text="Found Something", height=10, width=20, command=None).place(x=100, y=50)
+b2 = Button(root, text="Retrieve Something", height=10, width=20, command=None).place(x=350, y=50)
+
+root.mainloop()
