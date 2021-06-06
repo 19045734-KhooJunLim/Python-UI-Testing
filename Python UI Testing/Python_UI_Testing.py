6286c08467f6be6d52e6f205a2994160dd521784
19045734 <19045734@myrp.edu.sg>
2021-06-06 17:23:27 +0800

    New UI that Jeff rewrote

--- Python UI Testing/Python_UI_Testing.py
+++ Python UI Testing/Python_UI_Testing.py
@@ -1,130 +1,128 @@
 from tkinter import *
+from tkinter import messagebox
+import pypyodbc
 
-
-class App(Tk):
+class Application(Tk):
     def __init__(self, *args, **kwargs):
         Tk.__init__(self, *args, **kwargs)
+        self.app_data = {}
         
-        container = Frame(self)
-        container.pack(side="top", fill="both", expand=True)
-        container.grid_rowconfigure(0,weight=1)
-        container.grid_columnconfigure(0,weight=1)
+        window=Frame(self)
+        window.pack()
+        window.grid_rowconfigure(0, minsize=500)
+        window.grid_columnconfigure(0, minsize=800)
+        self.T5 = StringVar()
         
-        self.frames = {}
+
+        self.frames={}
+        for f in(firstPage, secondPage_lost, thirdPage_found, fourPage_found, fivePage_lost):
+            frame=f(window, self)
+            self.frames[f]=frame
+            frame.grid(row=0, column=0,sticky="nsew")
+        self.show_frame(firstPage)
         
-        for F in (StartPage, PageOne, PageTwo,PageFour):
-            frame = F(container, self)
-            self.frames[F] = frame
-            frame.grid(row=0, column=0, sticky="NSEW")
-            
-            self.show_frame(StartPage)
-    def show_frame(self, context):
-        frame = self.frames[context]
+    def show_frame(self, page):
+        frame = self.frames[page]
         frame.tkraise()
-            
-            
+        self.title("Lost and Application")
 
-#start page coding
-class StartPage(Frame):
-    def __init__(self, parent,controller):
+    
+
+    
+class firstPage(Frame):
+    def __init__(self, parent, controller):
         Frame.__init__(self, parent)
+        border=LabelFrame(self, text="",bg="ivory",bd=10, font=("Arial",20))
+        border.pack(fill="both", expand="yes",padx=150,pady=100)
         
-        label = Label(self, text="Lost and Found")
-        label.pack(padx=10, pady=10)
+        l1=Label(border, text="Selection Page",font=("Arial bold",25))
+        l1.place(x=120, y=50)
         
+        bLost=Button(border, text="Lost an Item",font=("Arial",15), command=lambda:controller.show_frame(secondPage_lost))
+        bFound=Button(border, text="Found an Item ",font=("Arial",15), command=lambda:controller.show_frame(thirdPage_found))
+        bLost.place(x=160, y=100)
+        bFound.place(x=150, y=150)
         
-        button1 = Button(self, text="Lost",width=50, height=5,border=1, command=lambda:controller.show_frame(PageOne))
-        button1.pack()
-        button2= Button(self, text ="Found", width=50, height=5,border=1, command=lambda:controller.show_frame(PageTwo))
-        button2.pack()
-#Page 1
-class PageOne(Frame):
-    def __init__(self, parent,controller):
+#report lost
+class secondPage_lost(Frame):
+    def __init__(self, parent, controller):
         Frame.__init__(self, parent)
+        l1=Label(self, text="second",font=("Arial bold",30))
+        l1.place(x=230, y=230)
+        bHome=Button(self, text="Home",font=("Arial",15), command=lambda:controller.show_frame(firstPage))
+        bHome.place(x=650, y=450)
+    
         
-        label = Label(self, text="Page one")
-        label.pack(padx=10, pady=10)
-        OPTIONS = [
-            "select Option",
-            "Phone",
-            "Key",
-            "Wallet",
-            "Card",
-            
-                ]
-        master = self
-        variable = StringVar(master)
-        variable.set(OPTIONS[0]) # default value
-
-        w = OptionMenu(master, variable, *OPTIONS)
-        w.pack()
-        page_four= Button(self, text ="enter", command=lambda:controller.show_frame(PageFour))
-        Start_page= Button(self, text ="HomePage", command=lambda:controller.show_frame(StartPage))
-        page_four.pack()
-        Start_page.pack(side=BOTTOM)
-        Start_page.pack(side=RIGHT)
-         
-
-class PageTwo(Frame):
-    def __init__(self, parent,controller):
+#claim found        
+class thirdPage_found(Frame):
+    
+    def __init__(self, parent, controller):
         Frame.__init__(self, parent)
+        self.controller = controller
         
-        label = Label(self, text="Found Item")
-        label.pack(padx=10, pady=10)
+        border=LabelFrame(self, text="Form",bg="ivory",bd=10, font=("Arial",20))
+        border.pack(fill="both", expand="yes",padx=150, pady=100)
         
-        place = Label(self,text="Place found")
-        place.pack(side=LEFT)
-        place_e = Entry(self)
-        place_e.pack(side=LEFT)
+        l1=Label(border, text="Full name",font=("Arial",15)).place(x=20, y=15)
+        T1=Entry(border, width =30, bd=5)
+        T1.place(x=200,y=15)
+        l2=Label(border, text="contact number",font=("Arial",15)).place(x=20, y=55)
+        T2=Entry(border, width =30, bd=5)
+        T2.place(x=200,y=55)
+        l3=Label(border, text="Item Found",font=("Arial",15)).place(x=20, y=105)
+        T3=Entry(border, width =30, bd=5)
+        T3.place(x=200,y=105)
+        l4=Label(border, text="Description",font=("Arial",15)).place(x=20, y=155)
+        T4=Entry(border, width =30, bd=5)
+        T4.place(x=200,y=155)
+        l5=Label(border, text="Location of the item",font=("Arial",15)).place(x=20, y=205)
+        T5=Entry(border, width =30, bd=5)
+        T5.place(x=200,y=205)
+        self.controller.T5.set(T5)
         
-        item = Label(self,text="Item")
-        item.pack(side=LEFT)
-        item_e = Entry(self)
-        item_e.pack(side=LEFT)
-        
-        L1 = Label(self,text="Alalalalala")
-        L1.pack(side=LEFT)
-        E1 = Entry(self)
-        E1.pack(side=LEFT)
-
-        Start_page= Button(self, text ="HomePage", command=lambda:controller.show_frame(StartPage))
-        Start_page.pack(side=BOTTOM)
-        #Start_page.pack(side=RIGHT)
-
-class PageFour(Frame):
-    def __init__(self, parent,controller):
-        Frame.__init__(self, parent)
-        
-        label = Label(self, text="Page 4")
-        label.pack(padx=10, pady=10)
-        alist = ["00000001","00000002", "00000003"]
-        blist = ["Samsung S10", "Apple X", "Huawei Nova 7i"]
-        total = len(alist)
-        i = 0
-        listbox1 = Listbox(self)
-        listbox2 = Listbox(self)
-        scrollbar1 = Scrollbar(self)
-        scrollbar2 = Scrollbar(self)
-        scrollbar1.pack(side= LEFT, fill = BOTH)
-        scrollbar2.pack(side= RIGHT, fill = BOTH)
-        listbox1.pack(side = LEFT)
-        listbox2.pack (side = RIGHT)
-        for i in range(total):
-            listbox1.insert(END, alist[i])
-            listbox2.insert(END, blist[i])
-            i = i+1
-        listbox1.config(yscrollcommand = scrollbar1.set)
-        listbox2.config(yscrollcommand = scrollbar2.set)
-        
-        scrollbar1.config(command = listbox1.yview)
-        scrollbar2.config(command = listbox2.yview)
-        button1= Button(self, text ="HomePage", command=lambda:controller.show_frame(StartPage))
-        button1.pack(side=BOTTOM)
-
        
 
-app = App()
 
+        def verify():
+            if T1.get()=='' or T2.get()=='' or T3.get()=='' or T4.get()=='' or T5.get()=='':
+                messagebox.showinfo("Error", "Please ensure all blanks have been filled!")
+            else:
+                controller.show_frame(fourPage_found)
+            
+        
+        bHome=Button(self, text="Back",font=("Arial",15), command=lambda:controller.show_frame(firstPage)).place(x=100, y=450)
+        bNext=Button(self, text="Submit",font=("Arial",15), command=verify).place(x=600, y=450)
+        
+
+#verify code check from database when user want to retr
+class fourPage_found(Frame):
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        self.controller = controller
+        border=LabelFrame(self, text="Form",bg="ivory",bd=10, font=("Arial",20))
+        border.pack(fill="both", expand="yes")
+        label1 = Label(border, textvariable=self.controller.T5).place(x=20, y=15)
+        
+    
+        
+#        l1=Label(self, text="four",font=("Arial bold",30))
+#        l1.place(x=230, y=230)
+        
+        
+        
+        
+        bHome=Button(self, text="previous",font=("Arial",15), command=lambda:controller.show_frame(thirdPage_found))
+        bHome.place(x=100, y=450)
+#give code to user who submit the LOST item
+class fivePage_lost(Frame):
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        l1=Label(self, text="fifth",font=("Arial bold",30))
+        l1.place(x=230, y=230)
+        bHome=Button(self, text="previous",font=("Arial",15), command=lambda:controller.show_frame(firstPage))
+        bHome.place(x=100, y=450)
+        
+        
+
+app=Application()
 app.mainloop()
-        
-        
