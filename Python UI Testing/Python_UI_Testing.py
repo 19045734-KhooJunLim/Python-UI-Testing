b2edc60feb93b24aa1201bab857a9198a871f7ba
YuJieYJ <69341779+YuJieYJ@users.noreply.github.com>
2021-06-06 20:01:21 +0800

    Updated page 2

--- Python UI Testing/Python_UI_Testing.py
+++ Python UI Testing/Python_UI_Testing.py
@@ -1,6 +1,5 @@
 from tkinter import *
 from tkinter import messagebox
-import pypyodbc
 
 class Application(Tk):
     def __init__(self, *args, **kwargs):
@@ -47,11 +46,74 @@ class firstPage(Frame):
 class secondPage_lost(Frame):
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
-        l1=Label(self, text="second",font=("Arial bold",30))
-        l1.place(x=230, y=230)
-        bHome=Button(self, text="Home",font=("Arial",15), command=lambda:controller.show_frame(firstPage))
-        bHome.place(x=650, y=450)
-    
+        self.controller = controller
+        
+        border=LabelFrame(self, text="Form",bg="ivory",bd=10, font=("Arial",20))
+        border.pack(fill="both", expand="yes",padx=150, pady=100)
+        
+        l1=Label(border, text="Full name",font=("Arial",15)).place(x=20, y=15)
+        T1=Entry(border, width =30, bd=5)
+        T1.place(x=200,y=15)
+        l2=Label(border, text="Contact Number",font=("Arial",15)).place(x=20, y=65)
+        T2=Entry(border, width =30, bd=5)
+        data = IntVar();
+        entry = Entry(textvariable = data);
+        T2.place(x=200,y=65)
+        l3=Label(border, text="Item Found",font=("Arial",15)).place(x=20, y=115)
+        T3=Entry(border, width =30, bd=5)
+        T3.place(x=200,y=115)
+        l4=Label(border, text="Description",font=("Arial",15)).place(x=20, y=165)
+        T4=Entry(border, width =30, bd=5)
+        T4.place(x=200,y=165)
+        l5=Label(border, text="Location of the item",font=("Arial",15)).place(x=20, y=215)
+        T5=Entry(border, width =30, bd=5)
+        T5.place(x=200,y=215)
+        self.controller.T5.set(T5)
+        
+       
+        def entry_to_Int(value):
+
+            if value != int:
+                c = "failed"
+                return c
+            else:
+                a = []
+                b = ''
+                for i in value:
+                    ii = int(i);
+                    a.append(ii)
+                for i in a:
+                    b = b + str(i)
+                    c = int(b)
+                return c
+            
+
+        def verify():
+            if T1.get()=='' or T2.get()=='' or T3.get()=='' or T4.get()=='' or T5.get()=='':
+                messagebox.showinfo("Error", "Please ensure all blanks have been filled!")
+            else:
+                 z = entry_to_Int(T2.get());
+                 
+                 
+                 if z == "failed" :
+                    messagebox.showinfo("Error", "Please only enter numbers")
+                    z = entry_to_Int(T2.get());
+                    
+                 else: 
+                     z = str(z)
+
+                     if len(z) != 8:
+                        messagebox.showinfo("Error", "Please enter your 8 digit number")
+
+                     else:
+                        controller.show_frame(fourPage_found)
+            
+            
+            
+        
+        bHome=Button(self, text="Back",font=("Arial",15), command=lambda:controller.show_frame(firstPage)).place(x=100, y=450)
+        bNext=Button(self, text="Submit",font=("Arial",15), command=verify).place(x=600, y=450)
+        
         
 #claim found        
 class thirdPage_found(Frame):
@@ -66,7 +128,7 @@ class thirdPage_found(Frame):
         l1=Label(border, text="Full name",font=("Arial",15)).place(x=20, y=15)
         T1=Entry(border, width =30, bd=5)
         T1.place(x=200,y=15)
-        l2=Label(border, text="contact number",font=("Arial",15)).place(x=20, y=55)
+        l2=Label(border, text="Contact Number",font=("Arial",15)).place(x=20, y=55)
         T2=Entry(border, width =30, bd=5)
         T2.place(x=200,y=55)
         l3=Label(border, text="Item Found",font=("Arial",15)).place(x=20, y=105)
